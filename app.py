26f95980c72fa55e07ee8168e4d2c689796b2395
vijaysae-Ai <vijay.sae20@gmail.com>
2023-09-12 10:57:34 +0530

    all components fixed

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/home', methods=['GET'])
+@app.route('/')
 def home():
-    return "vijay"
+    return render_template("index.html")
 
-if __name__ == "__main__":
+@app.route('/resume')
+def resume():
+    return render_template("Resume.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/nlp_project')
+def nlp_project():
+    return render_template("nlp_project.html")
+
+@app.route('/movie_recommendation')
+def movie_recommendation():
+    return render_template("movie_recommendation.html")
+
+@app.route('/fraud_detection')
+def fraud_detection():
+    return render_template("fraud_detection.html")
+
+@app.route('/customer_churn_prediction')
+def customer_churn_prediction():
+    return render_template("customer_churn_prediction.html")
+
+@app.route('/nlp_for_customer_support')
+def nlp_for_customer_support():
+    return render_template("nlp_for_customer_support.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+if __name__ == '__main__':
     app.run(debug=True)
